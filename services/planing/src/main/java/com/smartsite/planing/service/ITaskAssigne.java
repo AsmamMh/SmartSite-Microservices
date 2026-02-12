c1d3b1279c34a4a5c6dc16d3ce76044f55a295f9
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-12 14:25:21 +0100

    update

--- services/planing/src/main/java/com/smartsite/planing/service/ITaskAssigne.java
+++ services/planing/src/main/java/com/smartsite/planing/service/ITaskAssigne.java
@@ -1,5 +1,10 @@
 package com.smartsite.planing.service;
 
+import org.apache.kafka.streams.processor.assignment.KafkaStreamsAssignment.AssignedTask;
+
 public interface ITaskAssigne {
-    
+    void deleteAssigneTAsk(Long id);
+    AssignedTask AddAssigneTAsk(AssignedTask assignedTask);
+    AssignedTask uPdateAssignedTask(AssignedTask assignedTask,Long id);
+    AssignedTask getAssigneTAskByID(Long id);
 }
--- services/planing/src/main/java/com/smartsite/planing/service/ITaskService.java
+++ services/planing/src/main/java/com/smartsite/planing/service/ITaskService.java
@@ -1,5 +1,10 @@
 package com.smartsite.planing.service;
 
+import com.smartsite.planing.domain.entity.Task;
+
 public interface ITaskService {
-    
+    void DeleteTAsk(Long id);
+    Task AddTAsk(Task task);
+    Task updateTask(Task Task,Long id);
+    Task getTaskById(Long id);
 }
