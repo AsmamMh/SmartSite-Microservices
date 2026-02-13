76d17fe356ca933f1c0dc46e102fe9feb605b0d9
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-13 21:07:12 +0100

    feat: add notification entity with fields for NotificationType, Message, ReceivedDate, and Receiver

---
+++ services/notification/src/main/java/com/smartsite/notification/notification/notification.java
@@ -0,0 +1,27 @@
+package com.smartsite.notification.notification;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class notification {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Long id;
+ private String NotificationType;
+ private String Message;
+ private String ReceivedDate;
+ private String Receiver;
+ 
+}
