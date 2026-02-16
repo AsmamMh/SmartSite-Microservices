035d85e233ee48ebe22a60ca464144d7895767b8
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-16 23:55:39 +0100

    feat: create TaskDTO class with relationships for task management

---
+++ services/planing/src/main/java/com/smartsite/planing/domain/entity/TaskDTO.java
@@ -0,0 +1,78 @@
+package com.smartsite.planing.domain.entity;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.smartsite.planing.domain.enums.TaskSTatus;
+
+import jakarta.persistence.CascadeType;
+
+import jakarta.persistence.Column;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "task")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private Duration esimatedDuration;
+
+    private LocalDateTime plannedStart;
+    private LocalDateTime plannedEnd;
+    private LocalDateTime actualStart;
+    private LocalDateTime actualEnd;
+
+    private Duration estimatedDuration;
+
+    @Enumerated(EnumType.STRING)
+    private TaskSTatus status = TaskSTatus.PLANNED;
+    private BigDecimal progress = BigDecimal.ZERO;
+    private String description;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "task_dependencies", joinColumns = @JoinColumn(name = "task_id"), inverseJoinColumns = @JoinColumn(name = "predecessor_id"))
+    private Set<Task> predecessors = new HashSet<>();
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private List<ResourceNeed> ressources = new ArrayList<>();
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private List<TaskAssigne> assignments = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    @JsonIgnore
+    private Project project;
+}
