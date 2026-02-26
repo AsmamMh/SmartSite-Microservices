7bcb8d48fccf911919324acc3ea4fad93d021eee
asma <asmamhamdi@gmail.com>
2026-02-26 21:30:45 +0100

    initial commit service gestion des chantiers

--- src/main/java/com/esprit/servicegestiondeschantier/ServicegestiondeschantierApplication.java
+++ src/main/java/com/esprit/servicegestiondeschantier/ServicegestiondeschantierApplication.java
@@ -2,8 +2,10 @@ package com.esprit.servicegestiondeschantier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServicegestiondeschantierApplication {
 
     public static void main(String[] args) {
