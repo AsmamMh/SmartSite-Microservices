3c7489d20dc5234b49afd31eabf8db925eea6dba
asma <asmamhamdi@gmail.com>
2026-02-27 00:07:46 +0100

    Configuration des microservices et mises à jour

--- services/asma/src/main/java/com/esprit/smartsite/SmartsiteApplication.java
+++ services/asma/src/main/java/com/esprit/smartsite/SmartsiteApplication.java
@@ -2,8 +2,10 @@ package com.esprit.smartsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SmartsiteApplication {
 
     public static void main(String[] args) {
