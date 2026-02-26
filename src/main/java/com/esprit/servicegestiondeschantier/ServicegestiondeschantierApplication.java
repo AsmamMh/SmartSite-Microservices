ada58d9cce97c501fbde607b07279b9924e4bb00
asma <asmamhamdi@gmail.com>
2026-02-26 21:11:05 +0100

    crud chantier

---
+++ src/main/java/com/esprit/servicegestiondeschantier/ServicegestiondeschantierApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.servicegestiondeschantier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicegestiondeschantierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicegestiondeschantierApplication.class, args);
+    }
+
+}
