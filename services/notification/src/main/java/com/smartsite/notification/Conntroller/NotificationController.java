8946d66e5e09c090a55a51c98e99730c44b41e0f
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-27 01:42:20 +0100

    Add notification service integration and update routing configurations
    - Implement notification routes in application.properties
    - Create NotificationDTO for data transfer
    - Add NotificationClient interface for Feign client communication
    - Update TaskAssignController to send notifications via Feign client
    - Remove RabbitMQ dependencies and related code
    - Update pom.xml files to use Spring Boot 3.5.11
    - Refactor ProjectController and TaskAssignController for cleaner code

--- services/notification/src/main/java/com/smartsite/notification/Conntroller/NotificationController.java
+++ services/notification/src/main/java/com/smartsite/notification/Conntroller/NotificationController.java
@@ -1,15 +1,21 @@
 package com.smartsite.notification.Conntroller;
 
+import java.util.Date;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smartsite.notification.notification.Notification;
 import com.smartsite.notification.notification.NotificationRepositorry;
+import com.smartsite.notification.dto.NotificationDTO;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -28,4 +34,22 @@ public class NotificationController {
         return this.notificationRepository.findAll();
     }
     
+    @PostMapping("/task-assignment")
+    public ResponseEntity<Void> receiveTaskAssignment(@RequestBody NotificationDTO notificationDTO) {
+        Notification notification = new Notification();
+        notification.setTitle(notificationDTO.getTitle());
+        notification.setDescription(notificationDTO.getDescription());
+        notification.setReceiver(notificationDTO.getWorkerId());
+        notification.setReceivedDate(new Date());
+        notification.setRead(false);
+        notification.setNotificationType(notificationDTO.getNotificationType() != null ? 
+            notificationDTO.getNotificationType() : "TASK_ASSIGNED");
+        
+        notificationRepository.save(notification);
+        System.out.println("Received task assignment: Task " + notificationDTO.getTaskId() + 
+            " assigned to worker " + notificationDTO.getWorkerId() + 
+            " for project " + notificationDTO.getProjectId());
+        
+        return ResponseEntity.ok().build();
+    }
 }
--- services/planing/src/main/java/com/smartsite/planing/Controllers/TaskAssignController.java
+++ services/planing/src/main/java/com/smartsite/planing/Controllers/TaskAssignController.java
@@ -3,14 +3,11 @@ package com.smartsite.planing.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.smartsite.planing.domain.entity.Task;
+import com.smartsite.planing.client.NotificationClient;
+import com.smartsite.planing.domain.DTO.NotificationDTO;
 import com.smartsite.planing.domain.entity.TaskAssigne;
-import com.smartsite.planing.rabbitmq.RabbitMQConfig;
 import com.smartsite.planing.service.ITaskAssigne;
 
-import lombok.NoArgsConstructor;
-
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,30 +15,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
 @RequestMapping("/api/taskassigne")
 @CrossOrigin(origins = "*")
 public class TaskAssignController {
+    
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private NotificationClient notificationClient;
 
-    public void sendMessage(String message) {
-        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "routing.key", message);
-    }
     private final ITaskAssigne taskAssigneService;
+    
     public TaskAssignController(ITaskAssigne taskAssigneService) {
         this.taskAssigneService = taskAssigneService;
     }
 
     @PostMapping("/{taskId}")
-    public ResponseEntity<TaskAssigne> postTaskAssign(@RequestBody TaskAssigne taskAssigne,@PathVariable Long taskId) {
-        TaskAssigne taskAssigne2= this.taskAssigneService.create(taskId, taskAssigne);
-        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "routing.key", taskAssigne2);
-        //sendMessage("Task assigned: " + taskAssigne.getWorkerId() + " to task " + taskId);
+    public ResponseEntity<TaskAssigne> postTaskAssign(@RequestBody TaskAssigne taskAssigne, @PathVariable Long taskId) {
+        TaskAssigne taskAssigne2 = this.taskAssigneService.create(taskId, taskAssigne);
+        
+        // Send notification via Feign client instead of RabbitMQ
+        NotificationDTO notificationDTO = new NotificationDTO();
+        notificationDTO.setTaskId(taskId);
+        notificationDTO.setWorkerId(taskAssigne2.getWorkerId());
+        notificationDTO.setProjectId(taskAssigne2.getTask() != null && taskAssigne2.getTask().getProject() != null ? 
+            taskAssigne2.getTask().getProject().getId() : null);
+        notificationDTO.setTitle("Task Assignment");
+        notificationDTO.setDescription("You have been assigned to task #" + taskId);
+        notificationDTO.setNotificationType("TASK_ASSIGNED");
+        
+        notificationClient.sendTaskAssignmentNotification(notificationDTO);
+        
         return ResponseEntity.ok(taskAssigne2);
     }
     
@@ -49,8 +53,4 @@ public class TaskAssignController {
     public ResponseEntity<TaskAssigne> getTaskAssign(@PathVariable Long taskid) {
         return ResponseEntity.ok(this.taskAssigneService.getById(taskid));
     }
-
-    
-
-    
 }
